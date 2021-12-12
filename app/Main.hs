b3888460a6ab50b0ed69f165af037d2a69f0276f
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-12-12 11:26:13 +0200

    Add Pangram example

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
 import LeapYear
+import Pangram
+import Data.Function
 
 main :: IO ()
 main = do
     print $ isLeapYear 2020
+    print $ isPangram ""
+    print $ 8.9 & max 6.7 & replicate 5 & sum
---
+++ src/Pangram.hs
@@ -0,0 +1,12 @@
+module Pangram (isPangram) where
+
+import qualified Data.Char as Char
+import qualified Data.List as List
+import Data.Function
+
+isPangram :: String -> Bool
+isPangram text =
+    let textInLowerCase = text & List.map Char.toLower
+    in ['a' .. 'z']
+    & List.all (`elem` textInLowerCase)
+
