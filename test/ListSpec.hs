d2e1a0089554f187c6c4309c9fc0a9449db2a380
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-12-12 16:02:05 +0200

    Fix test

--- test/ListSpec.hs
+++ test/ListSpec.hs
@@ -9,7 +9,7 @@ spec =
     it "groupTuples should return correct result" $
       let dic = [(1, "aa"), (1, "cc"), (2, "aa"), (3, "ff"), (3, "gg"), (1, "bb")]
           result = groupTuples dic
-       in result `shouldBe` [(1, ["aa", "bb", "cc"]), (2, ["aa"]), (3, ["ff", "gg"])]
+       in result `shouldBe` [(1, ["aa", "cc", "bb"]), (2, ["aa"]), (3, ["ff", "gg"])]
 
 main :: IO ()
 main = hspec spec
