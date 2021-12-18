483d4642b2308f3fd3c54a21319fd9499fd3c14a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-12-18 18:06:47 +0200

    Solution outline

--- src/Counting.hs
+++ src/Counting.hs
@@ -5,6 +5,7 @@ module Counting (
 ) where
 
 import Data.Set (Set)
+import Data.Function
 
 data Color = Black | White deriving (Eq, Ord, Show)
 type Coord = (Int, Int)
@@ -13,4 +14,17 @@ territories :: [String] -> [(Set Coord, Maybe Color)]
 territories board = error "You need to implement this function."
 
 territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
-territoryFor board coord = Nothing
+territoryFor board coord = 
+  coord
+  & traverse (getEmptyNeighbors board)
+  & getTerritoryWithBorderColors
+  & tryGetTerritoryWithOwner
+  where
+    traverse getSuccessors root =
+      error "You need to implement this function."
+    getEmptyNeighbors board coord = 
+      error "You need to implement this function."
+    getTerritoryWithBorderColors territoryWithBorders = 
+      error "You need to implement this function."
+    tryGetTerritoryWithOwner territoryWithBorderColors = 
+      error "You need to implement this function."
