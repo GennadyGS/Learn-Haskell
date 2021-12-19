60e26ae5ac1f8e53185f5162d95e2e6f20ce5d45
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-12-19 14:06:14 +0200

    Implement extractEmptyTerritoryAndColors

--- src/Counting.hs
+++ src/Counting.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Counting (
-    Color(..),
+module Counting
+  ( Color (..),
     territories,
-    territoryFor
-) where
+    territoryFor,
+  )
+where
 
-import Data.Set (Set)
 import Data.Function
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 data Color = Black | White deriving (Eq, Ord, Show)
 type Coord = (Int, Int)
@@ -16,24 +20,36 @@ territories :: [String] -> [(Set Coord, Maybe Color)]
 territories board = error "You need to implement this function."
 
 territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
-territoryFor board coord = 
+territoryFor board coord =
   coord
-  & searchForAllReachableNodes (getEmptyNeighbors board)
-  & extractEmptyTerritoryAndColors
-  & tryGetTerritoryWithOwner
+    & searchForAllReachableNodes (getEmptyNeighbors board)
+    & extractEmptyTerritoryAndColors board
+    & tryGetTerritoryWithOwner
   where
+    getColor :: [String] -> Coord -> Maybe Color
+    getColor =
+      error "You need to implement this function."
+
     searchForAllReachableNodes :: forall a. Ord a => (a -> Set a) -> a -> Set a
     searchForAllReachableNodes getSuccessors root =
       error "You need to implement this function."
-    
+
     getEmptyNeighbors :: [String] -> Coord -> Set Coord
-    getEmptyNeighbors board coord = 
+    getEmptyNeighbors board coord =
       error "You need to implement this function."
-    
-    extractEmptyTerritoryAndColors :: Set Coord -> (Set Coord, Set Color)
-    extractEmptyTerritoryAndColors territory = 
-      error "You need to implement this function."
-    
+
+    extractEmptyTerritoryAndColors :: [String] -> Set Coord -> (Set Coord, Set Color)
+    extractEmptyTerritoryAndColors board territory =
+      let (emptyIntersections, occupiedIntersections) =
+            territory
+              & Set.partition (const True)
+          owners =
+            occupiedIntersections
+              & Set.toList
+              >>= (Maybe.maybeToList . getColor board)
+              & Set.fromList
+       in (emptyIntersections, owners)
+
     tryGetTerritoryWithOwner :: (Set Coord, Set Color) -> Maybe (Set Coord, Maybe Color)
-    tryGetTerritoryWithOwner (coords, colors) = 
+    tryGetTerritoryWithOwner (coords, colors) =
       error "You need to implement this function."
